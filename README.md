423927e1e3e39fa3a067101c66416e17269dcc9b
Srijan Saurav <srijan@deepsource.io>
2023-08-21 15:50:55 +0530

    chore: Add more structures
    Still playing around. Nothing is final. Yet.

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # DeepSource Community Analyzers
 
 Hub of all open-sourced third-party static analyzers supported by DeepSource.
+
+## Supported Analyzers
+
+|Analyzer name|Latest version|Language / Technology|
+|:-----------|:------------|:-------------------|
+|[facebook/infer](https://github.com/facebook/infer)|v1.1.0|Java, C++, Objective-C|
+|[Azure/bicep](https://github.com/Azure/bicep)|v0.20.4|Azure Resource Manager|
+
+---
+## Development Guide
+...
+
+## Maintenance Guide
+...
---
+++ analyzers/infer/utils/issue_scraper.py
@@ -0,0 +1,36 @@
+"""
+This script is responsible to scrape issues from infer's documentation, and create separate md files
+for DeepSource to register them.
+
+Infer doesn't provide issue codes for the issues it detects, so we'd be using a mapping to take care
+of it. DeepSource expects an issue code for all issues.
+
+Note: The script heavily depends on the directory structure, and would break if the structure is
+changed. The maintainers should pay attention to making sure this is updated all the time.
+"""
+import pathlib
+import os
+import subprocess
+from tempfile import TemporaryDirectory
+
+
+ISSUES_FOLDER = os.path.join(os.path.abspath(__file__), "issues")
+ISSUE_MAP = ""
+CLONE_URL = "https://github.com/facebook/infer.git"
+
+
+def clone_issues_locally():
+    """Clone the issues directory from infer's GitHub repo."""
+    clone_command = f"git clone {CLONE_URL} --depth=1".split()
+    with TemporaryDirectory() as temp_dir_name:
+        subprocess.run(clone_command, cwd=temp_dir_name)
+        resp = subprocess.run(["ls", temp_dir_name])
+        infer_issues_dir = f"{temp_dir_name}/infer/infer/documentation/issues"
+        for filename in os.listdir(infer_issues_dir):
+            if not filename.endswith(".md"):
+                pass
+            infer_issue_id = filename.removesuffix(".md")
+
+        breakpoint()
+
+clone_issues_locally()
