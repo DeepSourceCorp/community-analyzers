f48e1447fb8c8866a70bc3bf732495c3b6bb996c
Tushar Sadhwani <tushar@deepsource.io>
2023-10-10 15:38:27 +0530

    fix mypy errors

--- sentry.py
+++ sentry.py
@@ -2,10 +2,11 @@ import logging
 import os
 
 import sentry_sdk
+import sentry_sdk.utils
 from sentry_sdk.integrations.logging import LoggingIntegration
 
 
-def initialize(disable_logging_exceptions=False):
+def initialize(disable_logging_exceptions: bool = False) -> None:
     """Initialize sentry."""
     sentry_integrations = []
     if disable_logging_exceptions:
@@ -19,16 +20,16 @@ def initialize(disable_logging_exceptions=False):
         integrations=sentry_integrations,
         default_integrations=False,
     )
-    sentry_sdk.init = lambda *_, **__: None
-    sentry_sdk.utils.MAX_STRING_LENGTH = 8192  # undocumented truncation length
+    sentry_sdk.init = lambda *_, **__: None  # type: ignore
+    sentry_sdk.utils.DEFAULT_MAX_VALUE_LENGTH = 8192  # type: ignore
 
 
-def raise_error(err):
+def raise_error(err: Exception) -> None:
     """Raise a sentry error."""
     sentry_sdk.capture_exception(err)
 
 
-def raise_info(message, context=None):
+def raise_info(message: str, context: dict[str, object] | None = None) -> None:
     """
     Capture a message and show in sentry as Info.
 
