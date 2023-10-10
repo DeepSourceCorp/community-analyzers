4f1dc9d95266c1f33b8d477840135250e676c664
Tushar Sadhwani <tushar@deepsource.io>
2023-10-10 15:40:43 +0530

    cleaner init

--- sentry.py
+++ sentry.py
@@ -6,19 +6,10 @@ import sentry_sdk.utils
 from sentry_sdk.integrations.logging import LoggingIntegration
 
 
-def initialize(disable_logging_exceptions: bool = False) -> None:
+def initialize() -> None:
     """Initialize sentry."""
-    sentry_integrations = []
-    if disable_logging_exceptions:
-        sentry_logging = LoggingIntegration(
-            level=logging.INFO,  # Capture info and above as breadcrumbs
-            event_level=None,
-        )
-        sentry_integrations.append(sentry_logging)
     sentry_sdk.init(
-        os.getenv("SENTRY_DSN"),
-        integrations=sentry_integrations,
-        default_integrations=False,
+        os.getenv("SENTRY_DSN"), integrations=[], default_integrations=False
     )
     sentry_sdk.init = lambda *_, **__: None  # type: ignore
     sentry_sdk.utils.DEFAULT_MAX_VALUE_LENGTH = 8192  # type: ignore
