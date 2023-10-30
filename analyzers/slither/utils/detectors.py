77a2d7547eb559c5790bf27f250f6e5f98422dd4
Eshaan Bansal <eshaan@deepsource.io>
2023-10-30 18:32:29 +0530

    feat: add slither

--- analyzers/slither/utils/detectors.py
+++ analyzers/slither/utils/detectors.py
@@ -1,11 +1,23 @@
 import inspect
-from typing import Dict, List, Type, Union
+from typing import Dict, List, Type, TypedDict
 
 from slither.detectors import all_detectors
 from slither.detectors.abstract_detector import AbstractDetector
 from slither.utils.command_line import output_detectors_json
 
 __all__ = ["get_all_detector_classes", "get_all_detector_json"]
+
+
+class DetectorJson(TypedDict):
+    index: int
+    check: str
+    title: str
+    impact: str
+    confidence: str
+    wiki_url: str
+    description: str
+    exploit_scenario: str
+    recommendation: str
 
 
 def get_all_detector_classes() -> List[Type[AbstractDetector]]:
@@ -16,7 +28,7 @@ def get_all_detector_classes() -> List[Type[AbstractDetector]]:
     ]
 
 
-def get_all_detector_json() -> List[Dict[str, Dict[str, Union[str, int]]]]:
+def get_all_detector_json() -> List[Dict[str, DetectorJson]]:
     # ref: https://gitbub.com/crytic/slither/blob/master/slither/utils/command_line.py#L321
     detector_classes = get_all_detector_classes()
     return output_detectors_json(detector_classes)
