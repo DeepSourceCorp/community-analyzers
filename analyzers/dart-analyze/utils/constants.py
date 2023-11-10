51ea0574b958339e91755843bc51340369c0ee0d
Parth <86726240+parth-deepsource@users.noreply.github.com>
2023-11-10 14:53:15 +0530

    feat: generate issue markdown files instead of toml

--- analyzers/dart-analyze/utils/constants.py
+++ analyzers/dart-analyze/utils/constants.py
@@ -5,3 +5,13 @@ ISSUE_PREFIX = "DRT-W"
 
 # The version of the supported tool
 VERSION = "stable"  # TODO: change v3.2.0 when tag is officially pushed
+
+ISSUE_MD_TEMPLATE = """---
+title: "{title}"
+verbose_name: "{verbose_name}"
+category: "{category}"
+weight: 70
+severity: "major"
+---
+{description}
+"""
--- analyzers/dart-analyze/utils/issue_gen.py
+++ analyzers/dart-analyze/utils/issue_gen.py
@@ -2,45 +2,42 @@ import os
 import tempfile
 from typing import List
 
+from constants import ISSUE_MD_TEMPLATE
 from extractor import Issue, IssueExtractor
 from issue_map_gen import generate_mapping
 
 
-def get_toml_content(title, description, category, verbose_name) -> str:
+def get_issue_content(title, description, category, verbose_name) -> str:
     """Return the content of the toml file."""
-    return f'''
-title = "{title}"
-verbose_name = "{verbose_name}"
-severity = "major"
-category = "{category}"
-weight = 70
-description = """
-{description}
-"""
-'''
-
-
-def get_issue_toml_filepath(issue_code: str) -> str:
-    return os.path.join(
-        os.path.dirname(os.path.dirname(__file__)),
-        f".deepsource/issues/{issue_code}.toml",
+    return ISSUE_MD_TEMPLATE.format(
+        title=title,
+        description=description,
+        category=category,
+        verbose_name=verbose_name,
     )
 
 
-def update_issue_tomls(issues: List[Issue]) -> None:
+def get_issue_filepath(issue_code: str) -> str:
+    return os.path.join(
+        os.path.dirname(os.path.dirname(__file__)),
+        f".deepsource/issues/{issue_code}.md",
+    )
+
+
+def update_issues(issues: List[Issue]) -> None:
     """Create/Update issue toml files."""
     # Generate new mapping
     mapping = generate_mapping(issues=issues)
 
     for issue in issues:
         issue_code = mapping[issue.code]["issue_code"]
-        filepath = get_issue_toml_filepath(issue_code)
-        contents = get_toml_content(
+        filepath = get_issue_filepath(issue_code)
+        content = get_issue_content(
             issue.title, issue.description, issue.category, issue.code
         )
 
         with open(filepath, "w") as f:
-            f.write(contents)
+            f.write(content)
 
 
 if __name__ == "__main__":
@@ -49,4 +46,4 @@ if __name__ == "__main__":
         extractor = IssueExtractor(d)
         issues = extractor.issues
 
-    update_issue_tomls(issues=issues)
+    update_issues(issues=issues)
