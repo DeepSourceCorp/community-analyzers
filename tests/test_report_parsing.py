e389428310eddcd35b80177db8495d1f80da86bb
Srijan Saurav <srijan@deepsource.io>
2023-11-14 18:09:21 +0530

    chore: fail tests if the test sarif reports have unsanitised issue codes

--- tests/test_report_parsing.py
+++ tests/test_report_parsing.py
@@ -3,9 +3,12 @@ import os
 import tempfile
 from contextlib import contextmanager
 from pathlib import Path
-from typing import Any, Dict, Iterator
+from typing import Any, Dict, Iterator, Set
 
 import run_community_analyzer
+
+import tomllib as toml
+import frontmatter # type: ignore[import-untyped]
 
 
 def make_artifact(report_path: str, workdir: str = "") -> str:
@@ -37,6 +40,28 @@ def parse_single_artifact(
     os.remove(output_path)
 
 
+def get_issue_codes_for_tool(report_tool: str) -> Set[str]:
+    """Return a set of valid issue codes for the given report tool."""
+    issues = set()
+    issues_dir_path = Path.joinpath(
+        Path(__file__).parent.parent, f"analyzers/{report_tool}/.deepsource/issues"
+    ).as_posix()
+    for issue_file in os.listdir(issues_dir_path):
+        issue_code, ext = issue_file.split(".", 1)
+        if ext == "toml":
+            parser = toml
+        else:
+            parser = frontmatter
+        issue_file_path = os.path.join(issues_dir_path, issue_file)
+        with open(issue_file_path) as fp:
+            issue_data = parser.loads(fp.read())
+            archived = issue_data.get("archived", False)
+            if not archived:
+                issues.add(issue_code)
+
+    return issues
+
+
 def test_sarif_parser() -> None:
     """End to end test to make sure the SARIF parser throws no errors while parsing reports."""
     # We parse all reports in `tests/fixtures/reports` and try to make a deepsource report out of it.
@@ -46,7 +71,22 @@ def test_sarif_parser() -> None:
     reports = os.listdir(reports_base_dir)
     for report in reports:
         report_tool = report.removesuffix(".sarif")
+        active_issues = get_issue_codes_for_tool(report_tool)
         report_path = Path(reports_base_dir, report).as_posix()
+
         with parse_single_artifact(report_path, report_tool) as result:
+            missing_ids = []
             assert result
             assert result["issues"] is not None
+            for issue in result["issues"]:
+                issue_code = issue["issue_code"]
+                if issue_code not in active_issues:
+                    missing_ids.append(issue_code)
+
+            if missing_ids:
+                raise AssertionError(
+                    f"{report_tool} report has {len(missing_ids)} missing issue "
+                    f"code(s): {', '.join(missing_ids)}\n"
+                    "Please make sure you add issue files for these issue codes in the "
+                    "respective issues directory."
+                )
