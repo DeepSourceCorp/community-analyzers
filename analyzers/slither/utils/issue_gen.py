fbdc5c28b6fe0a933a4267106edb0e4cb11908ed
Eshaan Bansal <eshaan@deepsource.io>
2023-11-22 16:30:54 +0530

    feat: add slither (#9)
    Slither: https://github.com/crytic/slither
    
    ---------
    
    Co-authored-by: Srijan Saurav <68371686+srijan-deepsource@users.noreply.github.com>

---
+++ analyzers/slither/utils/issue_gen.py
@@ -0,0 +1,99 @@
+import os
+from textwrap import dedent
+
+from constants import (
+    DEEPSOURCE_SEVERITY_WEIGHT_MAP,
+    SLITHER_DETECTOR_CLASSIFICATION_DEEPSOURCE_CATEGORY_MAP,
+    SLITHER_DETECTOR_CLASSIFICATION_DEEPSOURCE_SEVERITY_MAP,
+)
+from detectors import get_all_detector_json
+from issue_map_gen import generate_mapping, get_issue_map
+
+
+def _get_toml_path(issue_code: str) -> str:
+    """Returns the file path to a issue toml for given `issue_code`."""
+    return os.path.join(
+        os.path.dirname(os.path.dirname(__file__)),
+        f".deepsource/issues/{issue_code}.toml",
+    )
+
+
+def get_toml_content(
+    title: str,
+    verbose_name: str,
+    severity: str,
+    category: str,
+    weight: int,
+    description: str,
+    exploit_scenario: str,
+    recommendation: str,
+    learn_more: str,
+) -> str:
+    """Return the content of the toml file."""
+    exploit_scenario_section = (
+        f"\n\n## Exploit Scenario\n{exploit_scenario}" if exploit_scenario else ""
+    )
+    recommendation_section = (
+        f"\n\n## Recommendation\n{recommendation}" if recommendation else ""
+    )
+    learn_more_section = f"\n\n## Learn more\n{learn_more} on Slither's wiki."
+
+    content = f'''title = "{title}"
+verbose_name = "{verbose_name}"
+severity = "{severity}"
+category = "{category}"
+weight = {weight}
+description = """
+{description}
+
+<!--more-->{exploit_scenario_section}{recommendation_section}{learn_more_section}
+"""
+    '''
+
+    return dedent(content)
+
+
+def update_issue_tomls() -> None:
+    """Create issue toml files."""
+    # Generates mapping if doesn't exist and then read it
+    generate_mapping()
+    mapping = get_issue_map()
+
+    detectors = get_all_detector_json()
+
+    # For each Slither detector,
+    # create a DeepSource equivalent .toml issue file
+    for detector in detectors:
+        rule_id = detector["rule_id"]
+        issue_code = mapping[rule_id]["issue_code"]
+
+        filepath = _get_toml_path(issue_code)
+
+        title = detector["title"].removesuffix(".")
+        check_name = detector["check"]
+        impact = detector["impact"]
+        wiki_url = detector["wiki_url"]
+        description = detector["description"]
+        exploit_scenario = dedent(detector["exploit_scenario"])
+        recommendation = dedent(detector["recommendation"])
+        severity = SLITHER_DETECTOR_CLASSIFICATION_DEEPSOURCE_SEVERITY_MAP[impact]
+        category = SLITHER_DETECTOR_CLASSIFICATION_DEEPSOURCE_CATEGORY_MAP[impact]
+        weight = DEEPSOURCE_SEVERITY_WEIGHT_MAP[severity]
+
+        content = get_toml_content(
+            title=title,
+            verbose_name=check_name,
+            severity=severity,
+            category=category,
+            weight=weight,
+            description=description,
+            exploit_scenario=exploit_scenario,
+            recommendation=recommendation,
+            learn_more=f"[{check_name}]({wiki_url})",
+        )
+        with open(filepath, "w") as f:
+            f.write(content)
+
+
+if __name__ == "__main__":
+    update_issue_tomls()
---
+++ analyzers/slither/utils/issue_map_gen.py
@@ -0,0 +1,56 @@
+import itertools
+import json
+from typing import Dict, Generator
+
+from constants import ISSUE_MAP_FILE, ISSUE_PREFIX
+from detectors import get_all_detector_json
+
+__all__ = ["get_issue_map", "generate_mapping"]
+
+
+def _get_next_code(mapping: Dict[str, Dict[str, str]]) -> Generator[int]:
+    """Return the next available issue code."""
+    num_issues = len(mapping.keys())  # get the number of issues already in the mapping
+    next_code = 1001 + num_issues  # issue code series starts from `1001`
+    yield from itertools.count(next_code)
+
+
+def get_issue_map() -> Dict[str, Dict[str, str]]:
+    """Return the issue map."""
+    with open(ISSUE_MAP_FILE, "r") as f:
+        return json.load(f)
+
+
+def get_mapping() -> Dict[str, Dict[str, str]]:
+    """Generates and returns a dict mapping of DeepSource assigned issue codes to Slither's detector names."""
+    # Example mapping:
+    # {
+    #     "0-0-abiencoderv2-array": {"issue_code": "SLITHER-W0001"},
+    # }
+    issue_map = get_issue_map()
+    generate_code = _get_next_code(issue_map)
+    detectors = get_all_detector_json()
+
+    if len(detectors) > len(issue_map):
+        # if the no. of issues in the mapping is less than the no. of detectors,
+        # then generate the mapping only for the new detectors
+        for detector in detectors:
+            if detector["rule_id"] not in issue_map:
+                next_code = next(generate_code)
+                issue_map[detector["rule_id"]] = {
+                    "issue_code": f"{ISSUE_PREFIX}{next_code}"
+                }
+
+    return issue_map
+
+
+def generate_mapping() -> None:
+    """Generates and dumps a dict mapping of DeepSource assigned issue codes to Slither's detector names in `issue_map.json` file."""
+    issue_map = get_mapping()
+    # dump mapping into file
+    with open(ISSUE_MAP_FILE, "w") as f:
+        json.dump(issue_map, f, indent=4)
+
+
+if __name__ == "__main__":
+    generate_mapping()
