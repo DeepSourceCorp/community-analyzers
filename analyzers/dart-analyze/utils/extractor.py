4b83d56a23bc4032589956e7643608480a3f2b01
Parth <86726240+parth-deepsource@users.noreply.github.com>
2023-11-23 16:28:28 +0530

    feat: fetch diagnostic issues

--- analyzers/dart-analyze/utils/extractor.py
+++ analyzers/dart-analyze/utils/extractor.py
@@ -12,10 +12,86 @@ class Issue:
     category: str
 
 
+class DiagnosticRulesParser:
+    def __init__(self, text: str):
+        """
+        A simple wrapper that parses a diagnostic rules
+        from a markdown file.
+
+        Args:
+            text (str): The text content to parse
+        """
+        self.lines = text.replace("\r\n", "\n").split("\n")
+        self.pos = 0
+
+    @property
+    def exhausted(self):
+        """Whether the text content has been exhausted or not."""
+        return self.pos >= len(self.lines)
+
+    def next_line(self):
+        """Returns the next line to be parsed."""
+        return self.lines[self.pos]
+
+    def consume(self):
+        """Consumes the current line and moves to the next line."""
+        self.pos += 1
+
+    def get_issues(self) -> dict:
+        """Parses the the given text and extracts issue definitions."""
+        issues = []
+        while not self.exhausted:
+            line = self.next_line()
+
+            # All issue definitions start with issue code being a
+            # heading 3 and the issue code being on that line itself.
+            if line.startswith("### "):
+                code = line.strip("# \n")
+                title = ""
+                description = ""
+                self.consume()
+
+                # Everything between heading 3 and heading 4
+                # is the title of the issue
+                while not self.exhausted:
+                    line = self.next_line()
+                    if line.startswith("#"):
+                        break
+                    title += line.strip(" \n_")
+                    self.consume()
+
+                # Everything after heading 4 to the next heading 3
+                # i.e. the next issue definition can be considered
+                # to be the issue description
+                while not self.exhausted:
+                    line = self.next_line()
+                    if line.startswith("### "):
+                        break
+                    if line.startswith("#### Description"):
+                        self.consume()
+                        continue
+                    if "{% prettify dart tag=pre+code %}" in line:
+                        line = "```dart"
+                    elif "{% endprettify %}" in line:
+                        line = "```"
+                    description += f"{line}\n"
+                    self.consume()
+                description = description.strip()
+
+                issues.append(
+                    {"code": code, "title": title, "description": description}
+                )
+            else:
+                self.consume()
+
+        return issues
+
+
 class IssueExtractor:
-    ISSUES_URL = (
+    LINTER_RULES_URL = (
         "https://github.com/dart-lang/site-www/raw/main/src/_data/linter_rules.json"
     )
+    DIAGNOSTIC_RULES_URL = "https://raw.githubusercontent.com/dart-lang/site-www/main/src/tools/diagnostic-messages.md"
     GROUP_CATEGORY_MAP = {
         "style": "antipattern",
         "errors": "bug-risk",
@@ -38,43 +114,55 @@ class IssueExtractor:
         if len(self._issues) > 0:
             return self._issues
 
-        rules = self.fetch_rules()
-        self._issues = [
+        self._issues = self.fetch_rules()
+        return self._issues
+
+    def fetch_rules(self) -> List[Issue]:
+        """Fetches all dart analyze rules."""
+        return [*self.fetch_linter_rules(), *self.fetch_diagnostic_rules()]
+
+    def fetch_diagnostic_rules(self) -> List[Issue]:
+        """Fetches the list of diagnostic rules"""
+        response = urlopen(self.DIAGNOSTIC_RULES_URL)
+        if response.code != 200:
+            raise Exception(f"Failed to fetch diagnostic rules {response.code}")
+
+        rules = DiagnosticRulesParser(response.read().decode()).get_issues()
+        return [Issue(**rule, category="bug-risk") for rule in rules]
+
+    def fetch_linter_rules(self) -> List[Issue]:
+        """Fetches the list of linter rules."""
+        response = urlopen(self.LINTER_RULES_URL)
+        if response.code != 200:
+            raise Exception(f"Failed to fetch linter rules {response.code}")
+
+        rules = json.load(response)
+        return [
             Issue(
-                self.get_code(rule),
-                self.get_title(rule),
-                self.get_description(rule),
-                self.get_category(rule),
+                self.get_linter_rule_code(rule),
+                self.get_linter_rule_title(rule),
+                self.get_linter_rule_description(rule),
+                self.get_linter_rule_category(rule),
             )
             for rule in rules
             if rule["state"] == "stable"
         ]
 
-        return self._issues
-
-    def fetch_rules(self) -> dict:
-        """Fetches the list of issues."""
-        response = urlopen(self.ISSUES_URL)
-        if response.code != 200:
-            raise Exception(f"Failed to fetch rules {response.code}")
-
-        return json.loads(response.read())
-
     @classmethod
-    def get_code(cls, rule: dict) -> str:
+    def get_linter_rule_code(cls, rule: dict) -> str:
         """Extracts the rule code"""
         return rule["name"]
 
     @classmethod
-    def get_title(cls, rule: dict) -> str:
+    def get_linter_rule_title(cls, rule: dict) -> str:
         """Extract & sanitize the rule title."""
         return rule["description"].rstrip(".").replace('"', r"\"")
 
-    def get_description(cls, rule: dict) -> str:
+    def get_linter_rule_description(cls, rule: dict) -> str:
         """Extracts the description from the rule"""
         return rule["details"]
 
     @classmethod
-    def get_category(cls, rule: dict) -> str:
+    def get_linter_rule_category(cls, rule: dict) -> str:
         """Extracts and returns the category mapped to the rule"""
         return cls.GROUP_CATEGORY_MAP.get(rule["group"])
