0b90a9938e81730a19f27fd9fc34766d84ebe730
Vishnu Jayadevan <vishnu@deepsource.io>
2023-11-29 15:55:30 +0530

    fix: deepsource issues and docstrings

--- analyzers/cfn-lint/utils/issue_map_gen.py
+++ analyzers/cfn-lint/utils/issue_map_gen.py
@@ -1,11 +1,20 @@
+# To run: python issue_map_gen.py <directory>#
 import json
 import os
 import sys
+from typing import Dict
 
 
-def generate_json(directory):
+def generate_json(directory: str) -> None:
+    """
+    Generate a JSON file containing mappings of issue codes extracted from TOML files in a directory.
+
+    :param directory: The directory containing TOML files with issue codes.
+    :type directory: str
+    :return: None
+    """
     # Dictionary to hold the mappings
-    issue_codes = {}
+    issue_codes: Dict[str, dict] = {}
 
     # Iterate through each file in the directory
     for filename in os.listdir(directory):
@@ -17,7 +26,7 @@ def generate_json(directory):
             issue_codes[issue_code] = {"issue_code": f"CFLIN-{issue_code}"}
 
     # Convert dictionary to JSON
-    json_data = json.dumps(issue_codes, indent=4)
+    json_data: str = json.dumps(issue_codes, indent=4)
 
     # Write JSON data to a file in the current directory
     with open("issue_map.json", "w") as file:
@@ -30,5 +39,5 @@ if __name__ == "__main__":
         print("Usage: python script.py <directory>")
         sys.exit(1)
 
-    target = sys.argv[1]
+    target: str = sys.argv[1]
     generate_json(target)
